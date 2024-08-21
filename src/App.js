2dc5c27e541bdb92145a17e5ba831ab0707a5bcd
Faisal-pixel <adamsfaisal2003@gmail.com>
2024-08-22 00:39:58 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom"
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import Error from './pages/Error.component';
+const HatsPage = () => {
+  return (
+    <div>
+    <h1>HATS PAGE</h1>
+  </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+      <Route path='/' element={<Homepage />}/> 
+      <Route path="/shop/hats" element={<HatsPage />}/>
+      <Route path='*' element = {<Error />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import {useNavigate, useLocation} from "react-router-dom"
 import "./menu-item.styles.scss"
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({title, imageUrl, size, linkUrl}) => {
+    let navigate = useNavigate();
+    let location = useLocation();
+    console.log(location.pathname)
     return (
-        <div  className={`${size} menu-item`}>
-        <div className="background-image" 
-            style={{
-            backgroundImage: `url(${imageUrl})`
-            }} 
-        />
-        <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
+        <div  className={`${size} menu-item`} onClick={() => navigate(`${location.pathname}/${linkUrl}`)}>
+            <div className="background-image" 
+                style={{
+                backgroundImage: `url(${imageUrl})`
+                }} 
+            />
+            <div className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
-    </div>
     )
 }
 
